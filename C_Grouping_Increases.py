866b534d88140eb1f2b77fda6f528aefd3cbbdb7
shuzeyfa <shuzeyfa4@gmail.com>
2026-04-16 18:17:46 +0300

    additonal problems

---
+++ C_Grouping_Increases.py
@@ -0,0 +1,59 @@
+import sys, os
+import math
+from collections import defaultdict, deque, Counter
+from functools import lru_cache
+from bisect import bisect_right, bisect_left
+RANDOM = int.from_bytes(os.urandom(8), "big")
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+t = 1
+t = getInt()
+
+   
+          
+def solve():
+    n = getInt()
+    l = getIntList()
+    
+    
+    x = y = float("inf")
+    penality = 0
+    
+    for i in l:
+        if x > y:
+            x, y = y, x
+        
+        if i <= x:
+            x = i
+        elif i <= y:
+            y = i
+        else:
+            x = i
+            penality += 1
+                    
+    print(penality)
+    
+    
+                                  
+    
+
+
+          
+            
+               
+     
+                             
+    
+        
+                     
+    
+                      
+    
+for _ in range(t):
+    solve()
